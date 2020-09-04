c7922b4980691a9988b1e6386273035682577888
Thien Nguyen <newtj2@gmail.com>
2020-09-04 16:03:33 +0100

    tidy up task

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { CartSummary } from '../features/cart/CartSummary';
 import CartTotal from '../features/cart/CartTotal';
