6a3dd302ef68b8b6ba3795d0970faf24547f9996
Thien Nguyen <newtj2@gmail.com>
2020-09-08 10:41:24 +0100

    Added not found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <h1 className="float-left display-3 mr-4">404</h1>
+      <p className="text-muted float-left">The page you are looking for was not found.</p>
+    </div>
+  );
+};
+
+export default NotFound;
